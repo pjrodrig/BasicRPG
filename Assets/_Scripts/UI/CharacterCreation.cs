1da214d5128b5764564cb9372c211cbe5cea164b
Paul Rodrigue <paul.john.rodrigue@gmail.com>
2018-12-09 21:22:11 -0700

    start character creation

---
+++ Assets/_Scripts/UI/CharacterCreation.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CharacterCreation : MonoBehaviour {
+
+    public InputField nameInput;
+    public Dropdown colorDropdown;
+
+
+    void Start() {
+        InitColorDropdown();
+    }
+
+    void InitColorDropdown() {
+    }
+
+}
